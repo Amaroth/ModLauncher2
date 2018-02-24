6e83c476271d02b7945241c1ccca02a3b6879ab2
Amaroth <Amaroth7@seznam.cz>
2018-02-24 15:46:52 +0100

    Project structure organization, include libs and refferences.

---
+++ ModLauncher2/GUI/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace ModLauncher2.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        internal void LoadSettings()
+        {
+            
+        }
+    }
+}
